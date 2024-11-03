4e086813e7a412b2853ab374acc47a0bfa6481ae
Ben Bichler <benbichler2@gmail.com>
2024-11-03 17:07:14 +0200

    added playpause/next/previous buttons and changed the playlist

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const audio = document.getElementById("background-audio");
 const volumeControl = document.getElementById("volume");
 const currentTimeDisplay = document.getElementById("current-time");
 const totalTimeDisplay = document.getElementById("total-time");
+const playPauseButton = document.getElementById("play-pause-button");
+const prevButton = document.getElementById("play-pause-button");
+const nextButton = document.getElementById("play-pause-button");
+
+const playlist = [
+  "assets/audio/end-up-gone.mp3", // Track 1
+  "assets/audio/magic-johnson.mp3", // Track 2
+  "assets/audio/never-stop.mp3",
+];
+
+let currentTrackIndex = 0;
 
 // Set initial volume based on slider value
 audio.volume = volumeControl.value;
@@ -15,12 +26,24 @@ function formatTime(seconds) {
 
 // Function to play audio
 function playAudio() {
+  audio.play().catch((error) => {
+    console.log("Playback prevented:", error);
+  });
+  updateTotalTime();
+  startTimeUpdate(); // Start updating time display
+  playPauseButton.src = "assets/images/playpause.png"; // Update button image to pause
+}
+
+function pauseAudio() {
+  audio.pause();
+  playPauseButton.src = "assets/images/playpause.png";
+}
+
+function handlePlayPause() {
   if (audio.paused) {
-    audio.play().catch((error) => {
-      console.log("Playback prevented:", error);
-    });
-    updateTotalTime();
-    startTimeUpdate(); // Start updating time display
+    playAudio();
+  } else {
+    pauseAudio();
   }
 }
 
@@ -45,8 +68,31 @@ function startTimeUpdate() {
   }, 1000); // Update every second
 }
 
+function loadTrack(index) {
+  currentTrackIndex = index;
+  audio.src = playlist[currentTrackIndex];
+  audio.load();
+  playAudio();
+}
+
+function playNext() {
+  currentTrackIndex = (currentTrackIndex + 1) % playlist.length;
+  loadTrack(currentTrackIndex);
+}
+
+function playPrevious() {
+  currentTrackIndex =
+    (currentTrackIndex - 1 + playlist.length) % playlist.length;
+  loadTrack(currentTrackIndex);
+}
+
 // Add event listener to the volume control
 volumeControl.addEventListener("input", handleVolumeChange);
+
+// Add event listeners to the play/pause, previous and next buttons
+playPauseButton.addEventListener("click", handlePlayPause);
+prevButton.addEventListener("click", playPrevious);
+nextButton.addEventListener("click", playNext);
 
 // Add event listener to the document to play audio on any click
 document.addEventListener("click", playAudio);
